71802de00f2cfa4765cd290d15ac1152ffb24ba1
inkosiyehacker <97656297+wriday@users.noreply.github.com>
2024-09-02 18:08:50 +0530

    config.py

--- config.py
+++ config.py
@@ -23,7 +23,7 @@ class Config(object):
     # Get your own proxy from https://github.com/rg3/youtube-dl/issues/1091#issuecomment-230163061
     HTTP_PROXY = os.environ.get("HTTP_PROXY", "")
     # maximum message length in Telegram
-    MAX_MESSAGE_LENGTH = 40960
+    MAX_MESSAGE_LENGTH = 4096
     # set timeout for subprocess
     PROCESS_MAX_TIMEOUT = 3600
     # watermark file
